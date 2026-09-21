a5f8637c790416dd4eed9c4f762077af269e597c
João Victor Melo <64021770+JVM3L0@users.noreply.github.com>
2026-09-21 18:00:49 +0000

    feat: chance letters to symbols

--- boards/shields/prospector_adapter/src/layouts/operator/modifier_indicator.c
+++ boards/shields/prospector_adapter/src/layouts/operator/modifier_indicator.c
@@ -9,6 +9,7 @@
 #include <zmk/hid.h>
 
 #include <fonts.h>
+#include <symbols.h>
 #include <modifier_order.h>
 #include "display_colors.h"
 
@@ -83,21 +84,10 @@ ZMK_SUBSCRIPTION(widget_modifier_indicator, zmk_keycode_state_changed);
 ZMK_SUBSCRIPTION(widget_modifier_indicator, zmk_caps_word_state_changed);
 #endif
 
-static lv_obj_t *create_separator(lv_obj_t *parent) {
-    lv_obj_t *sep = lv_obj_create(parent);
-    lv_obj_set_size(sep, 2, 24);
-    lv_obj_set_style_bg_color(sep, lv_color_hex(DISPLAY_COLOR_MOD_SEPARATOR), LV_PART_MAIN);
-    lv_obj_set_style_bg_opa(sep, LV_OPA_COVER, LV_PART_MAIN);
-    lv_obj_set_style_border_width(sep, 0, LV_PART_MAIN);
-    lv_obj_set_style_radius(sep, 0, LV_PART_MAIN);
-    lv_obj_set_style_pad_all(sep, 0, LV_PART_MAIN);
-    return sep;
-}
-
-static lv_obj_t *create_mod_label(lv_obj_t *parent, const char *text) {
+static lv_obj_t *create_mod_label(lv_obj_t *parent, const char *symbol) {
     lv_obj_t *label = lv_label_create(parent);
-    lv_label_set_text(label, text);
-    lv_obj_set_style_text_font(label, &FG_Medium_20, LV_PART_MAIN);
+    lv_label_set_text(label, symbol);
+    lv_obj_set_style_text_font(label, &Symbols_Semibold_32, LV_PART_MAIN);
     lv_obj_set_style_text_color(label, lv_color_hex(DISPLAY_COLOR_MOD_INACTIVE), LV_PART_MAIN);
     return label;
 }
@@ -113,10 +103,7 @@ int zmk_widget_modifier_indicator_init(struct zmk_widget_modifier_indicator *wid
     lv_obj_set_flex_align(widget->obj, LV_FLEX_ALIGN_SPACE_BETWEEN, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER);
 
     for (int i = 0; i < 4; i++) {
-        widget->mod_labels[i] = create_mod_label(widget->obj, modifier_order_get_text(i));
-        if (i < 3) {
-            create_separator(widget->obj);
-        }
+        widget->mod_labels[i] = create_mod_label(widget->obj, modifier_order_get_symbol(i));
     }
 
     sys_slist_append(&widgets, &widget->node);
