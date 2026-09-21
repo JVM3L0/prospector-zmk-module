38e111b4224171c10066a48100ea94bfb982641a
João Victor Melo <64021770+JVM3L0@users.noreply.github.com>
2026-09-21 17:53:25 -0300

    Update modifier_indicator.c

--- boards/shields/prospector_adapter/src/layouts/operator/modifier_indicator.c
+++ boards/shields/prospector_adapter/src/layouts/operator/modifier_indicator.c
@@ -13,7 +13,7 @@
 #include <modifier_order.h>
 #include "display_colors.h"
 
-LV_FONT_DECLARE(Symbols_Semibold_32);
+LV_FONT_DECLARE(Symbols_Semibold_28);
 
 static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
 
@@ -89,12 +89,8 @@ ZMK_SUBSCRIPTION(widget_modifier_indicator, zmk_caps_word_state_changed);
 static lv_obj_t *create_mod_label(lv_obj_t *parent, const char *symbol) {
     lv_obj_t *label = lv_label_create(parent);
     lv_label_set_text(label, symbol);
-    lv_obj_set_style_text_font(label, &Symbols_Semibold_32, LV_PART_MAIN);
+    lv_obj_set_style_text_font(label, &Symbols_Semibold_28, LV_PART_MAIN);
     lv_obj_set_style_text_color(label, lv_color_hex(DISPLAY_COLOR_MOD_INACTIVE), LV_PART_MAIN);
-
-    /* Reduz a escala visual do ícone (256 = 100%, 192 = 75%, 160 = ~62%) */
-    lv_obj_set_style_transform_zoom(label, 192, LV_PART_MAIN);
-
     return label;
 }
 
